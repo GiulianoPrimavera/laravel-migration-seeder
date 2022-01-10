c98ac79d1ce2690218b4be3c973904d3d5b9143e
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-10 17:08:24 +0100

    fixes seeder problems

--- database/seeds/PackTableSeeder.php
+++ database/seeds/PackTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Pack;
 
 class PackTableSeeder extends Seeder
 {
@@ -10,8 +11,16 @@ class PackTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $singleTripPack = new Pack();
+            $singleTripPack->name = $faker->word;
+            $singleTripPack->location = $faker->city;
+            $singleTripPack->duration = $faker->randomFloat($nbMaxDecimal = 0, $min = 0, $max = 20);
+            $singleTripPack->period = $faker->monthName();
+            $singleTripPack->price = $faker->numberBetween($min = 30, $max = 5000);
+            $singleTripPack->save();
+        }
     }
 }
